506c0a052228c414ec513ef33147a732ff7199d0
mortinsz <72875844+mortinsz@users.noreply.github.com>
2021-03-02 14:07:39 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		slidesToShow: 2,
+		autoplay: true, 
+		autoplaySpeed: 2000,
+		infinite: true,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+	});
+})
